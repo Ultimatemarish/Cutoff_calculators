2354e57461249e122b3bd3e18936d14f1e4a0107
Ultimatemarish <112646909+Ultimatemarish@users.noreply.github.com>
2022-09-10 20:01:47 +0530

    TNAU CUTOFF

---
+++ TNAU cutoff.py
@@ -0,0 +1,6 @@
+p=int(input("Enter ur physics marks for 100:"))
+c=int(input("Enter ur chemistry marks for 100:"))
+m=int(input("Enter ur maths marks for 100:"))
+b=int(input("Enter ur biology marks for 100:"))
+cmarks=(p+c+m+b)/2
+print("CONGRATULATIONS!,YOUR CUTOFF MARKS FOR AGRICULTURAL ADMISSION IS",cmarks)
