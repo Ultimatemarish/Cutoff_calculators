a1eb6a9485d9d95a8f8fade5ae03bf6708a16127
Ultimatemarish <112646909+Ultimatemarish@users.noreply.github.com>
2022-09-10 19:43:25 +0530

    Here is the code for tnea  cutoff calulation!

---
+++ tnea cut off calc.py
@@ -0,0 +1,5 @@
+p=int(input("Enter ur physics marks for 100:"))
+c=int(input("Enter ur chemistry mark for 100:"))
+m=int(input("Enter ur maths mark for 100:"))
+cmarks=p/2+c/2+m
+print("YOUR CUT OFF MARKS FOR TNEA IS",cmarks)
